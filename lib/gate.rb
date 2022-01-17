6c84585ae48add500ea71cf468bb8cbdb88b2eb0
yu-M215 <yuu0609km.215@gmail.com>
2022-01-17 12:24:35 +0000

    改札機プログラムの作成　Ticketクラス作成

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,2 +1,5 @@
 class Gate
+  def initialize(name)
+    @name = name
+  end
 end
--- test/gate_test.rb
+++ test/gate_test.rb
@@ -1,9 +1,14 @@
 require 'minitest/autorun'
 require './lib/gate'
+require './lib/ticket'
 
 class GateTest < Minitest::Test
   def test_gate
-    # とりあえずGateオブジェクトが作れることを確認する
-    assert Gate.new
+    # 梅田から十三まで150円の切符で出場できるか
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    assert juso.exit(ticket)
   end
 end
