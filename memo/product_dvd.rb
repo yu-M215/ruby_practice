4e9334dd6b9841d074a3bce68c27b03e6d9fb6af
yu-M215 <yuu0609km.215@gmail.com>
2022-01-18 06:48:18 +0000

    引数の数が同じ場合のsuperの使い方

--- memo/product_dvd.rb
+++ memo/product_dvd.rb
@@ -16,16 +16,23 @@ class DVD < Product
   # nameとpriceはスーパークラスでattr_readerが設定されているので定義不要
   attr_reader :running_time
 
-  def initialize(name, price, running_time)
-    # スーパークラスのinitializeメソッドを呼び出す
-    super(name, price)
-    # DVDクラス独自の属性
-    @running_time = running_time
+  # def initialize(name, price, running_time)
+  #   # スーパークラスのinitializeメソッドを呼び出す
+  #   super(name, price)
+  #   # DVDクラス独自の属性
+  #   @running_time = running_time
+  # end
+
+  def initialize(name, price)
+    # 引数をすべてスーパークラスのメソッドに渡す。super(name, price)と書いたのと同じ
+    super
+    # サブクラスで必要な初期化処理を書く
   end
 end
 
-dvd = DVD.new('A great movie', 1000, 120)
+# dvd = DVD.new('A great movie', 1000, 120)
+dvd = DVD.new('A great movie', 1000)
 
 puts dvd.name
 puts dvd.price
-puts dvd.running_time
+# puts dvd.running_time
