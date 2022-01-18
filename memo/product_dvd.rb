bf5430374e8bdbfbefaea9bf640ca6ada7d62825
yu-M215 <yuu0609km.215@gmail.com>
2022-01-18 06:57:21 +0000

    superなしでもスーパークラスのinitializeメソッドが呼び出されることの確認

--- memo/product_dvd.rb
+++ memo/product_dvd.rb
@@ -25,18 +25,22 @@ class DVD < Product
 
   # def initialize(name, price)
   #   # 引数をすべてスーパークラスのメソッドに渡す。super(name, price)と書いたのと同じ
+  #   # サブクラスで特別な処理をしないなら、同名メソッドを定義する必要はない
+  #   # (スーパークラスに処理を任せる)
   #   super
   #   # サブクラスで必要な初期化処理を書く
   # end
 
-  def initialize(name, price)
-    # super()だと引数なしでスーパークラスのメソッドを呼び出す
-    # (ただし数が合わないのでこのコードはエラーになる)
-    super()
-  end
+  # def initialize(name, price)
+  #   # super()だと引数なしでスーパークラスのメソッドを呼び出す
+  #   # (ただし数が合わないのでこのコードはエラーになる)
+  #   super()
+  # end
 end
 
 # dvd = DVD.new('A great movie', 1000, 120)
+
+# initializeを記述しない状態でDVDクラスをnewすると、自動的にスーパークラスのinitializeメソッドが呼び出される
 dvd = DVD.new('A great movie', 1000)
 
 puts dvd.name
