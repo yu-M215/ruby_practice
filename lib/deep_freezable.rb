7cb6d875be1e7486c5ac4a073abee9e536044e4a
yu-M215 <yuu0609km.215@gmail.com>
2022-01-20 14:46:33 +0000

    deep_freeze.rbの配列向けのdeep_freezeメソッドの実装

--- lib/deep_freezable.rb
+++ lib/deep_freezable.rb
@@ -1,5 +1,10 @@
 module DeepFreezable
   def deep_freeze(array_or_hash)
-    # 実装はあとで
+    # 配列の各要素をfreeze
+    array_or_hash.each do |element|
+      element.freeze
+    end
+    # 配列自身をfreeze（かつ、これがメソッドの戻り値となる）
+    array_or_hash.freeze
   end
 end
