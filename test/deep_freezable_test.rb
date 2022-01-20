1c28af3fee08a5066ff2f23db91ab613a8d4d05a
yu-M215 <yuu0609km.215@gmail.com>
2022-01-20 14:51:46 +0000

    deep_freezable_test.rb更新　ハッシュ向けのテスト実装

--- test/deep_freezable_test.rb
+++ test/deep_freezable_test.rb
@@ -11,4 +11,16 @@ class DeepFreezableTest < Minitest::Test
     # 配列の要素がすべてfreezeされているか？
     assert Team::COUNTRIES.all? { |country| country.frozen? }
   end
+
+  def test_deep_freeze_to_hash
+    # ハッシュの値は正しいか？
+    assert_equal(
+      { 'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee' },
+      Bank::CURRENCIES
+    )
+    # ハッシュ自身がfreezeされているか？
+    assert Bank::CURRENCIES.frozen?
+    # ハッシュの要素（キーと値）がすべてfreezeされているか？
+    assert Bank::CURRENSIES.all? { |key, value| key.frozen? && value.frozen? }
+  end
 end
