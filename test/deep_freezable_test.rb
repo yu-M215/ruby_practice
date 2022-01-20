71e12bcdf9073b16e7706e5ccff59327a812d97c
yu-M215 <yuu0609km.215@gmail.com>
2022-01-20 14:44:02 +0000

    deep_freezable_test.rbの配列向けのテストコード作成

--- test/deep_freezable_test.rb
+++ test/deep_freezable_test.rb
@@ -3,8 +3,12 @@ require './lib/bank'
 require './lib/team'
 
 class DeepFreezableTest < Minitest::Test
-  def test_deep_freeze
-    # とりあえずモジュールが参照できることを確認する
-    assert DeepFreezable
+  def test_deep_freeze_to_array
+    # 配列の値は正しいか？
+    assert_equal ['Japan', 'US', 'India'], Team::COUNTRIES
+    # 配列自身がfreezeされているか？
+    assert Team::COUNTRIES.frozen?
+    # 配列の要素がすべてfreezeされているか？
+    assert Team::COUNTRIES.all? { |country| country.frozen? }
   end
 end
