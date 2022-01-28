ca6221ffed7df6d482f4da1832bc645d410e7591
yu-M215 <yuu0609km.215@gmail.com>
2022-01-28 05:59:19 +0000

    reverseメソッドの実装

--- lib/effects.rb
+++ lib/effects.rb
@@ -1,5 +1,7 @@
 module Effects
   def self.reverse
-    # 実装は後で
+    ->(words) do
+      words.split(' ').map(&:reverse).join(' ')
+    end
   end
 end
--- test/effects_test.rb
+++ test/effects_test.rb
@@ -3,7 +3,7 @@ require './lib/effects'
 
 class EffectsTest < Minitest::Test
   def test_reverse
-    # とりあえずモジュールが参照できることを確認する
-    assert Effects
+    effect = Effects.reverse
+    assert_equal 'ybuR si !nuf', effect.call('Ruby is fun!')
   end
 end
