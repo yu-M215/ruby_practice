f3301a4b45dcd808abd580c6cebea59d8ebbd6ec
yu-M215 <yuu0609km.215@gmail.com>
2022-01-28 06:03:42 +0000

    echoメソッドの実装

--- lib/effects.rb
+++ lib/effects.rb
@@ -4,4 +4,12 @@ module Effects
       words.split(' ').map(&:reverse).join(' ')
     end
   end
+
+  def self.echo(rate)
+    ->(words) do
+      # スペースならそのまま返す
+      # スペース以外ならその文字を指定された回数だけ繰り返す
+      words.chars.map { |c| c == ' ' ? c : c * rate }.join
+    end
+  end
 end
