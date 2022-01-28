dc24e8c3b95acdeefda3376b01cc7c7c372c85dc
yu-M215 <yuu0609km.215@gmail.com>
2022-01-28 06:07:41 +0000

    loudメソッドの実装

--- lib/effects.rb
+++ lib/effects.rb
@@ -12,4 +12,11 @@ module Effects
       words.chars.map { |c| c == ' ' ? c : c * rate }.join
     end
   end
+
+  def self.loud(level)
+    ->(words) do
+      # スペースで分割 > 大文字変換と"!"の付与 > スペースで連結
+      words.split(' ').map { |word| word.upcase + '!' * level }.join(' ')
+    end
+  end
 end
